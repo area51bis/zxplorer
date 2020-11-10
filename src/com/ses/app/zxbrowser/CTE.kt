064cadd04e63df631eda6834d194ee5798e238bc
area51bis <area51bis@gmail.com>
2020-11-10 17:47:01 +0100

    [*] Cambio de versión.

--- src/com/ses/app/zxbrowser/CTE.kt
+++ src/com/ses/app/zxbrowser/CTE.kt
@@ -1,6 +1,6 @@
 package com.ses.app.zxbrowser
 
 const val APP_NAME = "ZXBrowser"
-const val APP_VERSION_NAME = "0.7"
+const val APP_VERSION_NAME = "0.7.1"
 const val FULL_APP_NAME = "$APP_NAME $APP_VERSION_NAME"
 const val COPYRIGHT = "2020 sés"
